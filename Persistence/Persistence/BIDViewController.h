be6e116059d0c37aa5678eeb47b9f76057ee55a5
Jovi Nel <jnp.parilla@gmail.com>
2013-12-05 14:58:38 +0800

    First commit, initialize files for the project
    Signed-off-by: Jovi Nel <jnp.parilla@gmail.com>

---
+++ Persistence/Persistence/BIDViewController.h
@@ -0,0 +1,21 @@
+//
+//  BIDViewController.h
+//  Persistence
+//
+//  Created by Maria Alice C. Lim on 12/5/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *field1;
+@property (weak, nonatomic) IBOutlet UITextField *field2;
+@property (weak, nonatomic) IBOutlet UITextField *field3;
+@property (weak, nonatomic) IBOutlet UITextField *field4;
+
+- (NSString *)dataFilePath;
+- (void)applicationWillResignActive:(NSNotification *)notification;
+
+@end
