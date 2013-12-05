cb2652b9168957d8296d7e0a2803c77e0650b3c8
Jovi Nel <jnp.parilla@gmail.com>
2013-12-05 16:22:01 +0800

    Used archiver to store and fetch data
    Signed-off-by: Jovi Nel <jnp.parilla@gmail.com>

---
+++ Persistence/Persistence/BIDFourLines.h
@@ -0,0 +1,18 @@
+//
+//  BIDFourLines.h
+//  Persistence
+//
+//  Created by Maria Alice C. Lim on 12/5/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BIDFourLines : NSObject <NSCoding, NSCopying>
+
+@property (copy, nonatomic) NSString *field1;
+@property (copy, nonatomic) NSString *field2;
+@property (copy, nonatomic) NSString *field3;
+@property (copy, nonatomic) NSString *field4;
+
+@end
